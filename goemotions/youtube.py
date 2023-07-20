401e4bc900cb8ada4962e9f7d5dd4ab262b7ea63
Sangchu <lopinjag7@gmail.com>
2023-07-20 15:15:16 +0900

    fix youtube

--- goemotions/youtube.py
+++ goemotions/youtube.py
@@ -1,25 +1,8 @@
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from oauth2client.tools import argparser
-from IPython.display import YouTubeVideo
-
-DEVELOPER_KEY = 'AIzaSyDvoocMqgF3xxDHjklcX9mUkVK6Y9dCGEM'
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-
-youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
-
+from ytmusicapi import YTMusic
 
 def load_youtube(artist, name):
-    search_response = youtube.search().list(
-        q = artist + name,
-        part = "snippet",
-        maxResults = 1
-        ).execute()
-
-    for item in search_response["items"]:
-        video_id = item["id"]["videoId"]
-        video_SD = "https://www.youtube.com/watch?v="+video_id
+    yt = YTMusic('oauth.json')
+    search_results = yt.search(artist +'의'+ name)
+    video_SD = "https://www.youtube.com/watch?v="+search_results[0]['videoId']
     
     return video_SD
-
