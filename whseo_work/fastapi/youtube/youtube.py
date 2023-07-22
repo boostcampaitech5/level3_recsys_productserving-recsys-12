fb5b025406c704c404dcd49c7ea75cde6a4d5944
codenee <tanedoran@gmail.com>
2023-07-23 08:12:00 +0900

    update youtube.py

--- whseo_work/fastapi/youtube/youtube.py
+++ whseo_work/fastapi/youtube/youtube.py
@@ -1,7 +1,8 @@
 from ytmusicapi import YTMusic
+from youtube_config import OAUTH_PATH
 
 def load_youtube(artist, name):
-    yt = YTMusic('E:/Python_Workspace/Web/WebProject/react-fastapi/fastapi/youtube/oauth.json')#yt = YTMusic('oauth.json')
+    yt = YTMusic(OAUTH_PATH)#yt = YTMusic('oauth.json')
     search_results = yt.search(artist +'의'+ name)
     print(search_results)
     video_SD = "https://www.youtube.com/watch?v="+search_results[0]['videoId']
