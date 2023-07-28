c3a2b19ad4186aebbf5472d76eea4ba556a5845a
Sangchu <88162218+Sangchu77@users.noreply.github.com>
2023-07-28 12:08:50 +0900

    Delete streamlit directory

--- streamlit/youtube.py
+++
@@ -1,8 +0,0 @@
-from ytmusicapi import YTMusic
-
-def load_youtube(artist, name):
-    yt = YTMusic('oauth.json')
-    search_results = yt.search(artist +'의'+ name)
-    video_SD = "https://www.youtube.com/watch?v="+search_results[0]['videoId']
-    
-    return video_SD
